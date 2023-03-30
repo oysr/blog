57b526b1cea9fc1542ebbc611970e032c1d109e4
oysr <940512860@qq.com>
2023-03-30 16:34:31 +0800

    增加媒体查询

---
+++ src/views/HomePage/components/PostCard/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="post-card">
+        <div class="img-box">
+            <img :src="img" alt="">
+        </div>
+        <div class="info"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PostCard',
+  data () {
+    return {
+      img: require('@/assets/img/img01.jpg')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.post-card {
+    position: relative;
+    text-align: left;
+    .img-box {
+        display: inline-block;
+        width: 680px;
+        height: 440px;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .info {
+        width: 500px;
+        height: 400px;
+        position: absolute;
+        top: 20px;
+        left: 660px;
+        padding: 0 80px;
+    }
+}
+</style>
--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -3,20 +3,25 @@
     <div class="cover" @mousemove="bgcMove">
         <div class="scene">
             <div class="layer" :style="layerStyle">
-                <img ref="bgc" :src="bgc" alt="" :width="bgcStyle.width">
+                <img ref="bgc" :src="bgc" alt="" :width="bgcStyle.width" class="bgc">
             </div>
         </div>
         <div class="wrap"></div>
     </div>
     <div class="content">
-        <div class="post"></div>
+        <post-card></post-card>
     </div>
   </div>
 </template>
 
 <script>
+import PostCard from './components/PostCard'
+
 export default {
   name: 'HomePage',
+  components: {
+    PostCard
+  },
   data () {
     return {
       bgc: require('@/assets/img/homepage.jpg')
@@ -39,7 +44,7 @@ export default {
   methods: {
     bgcMove (event) {
       const x = (event.offsetX * -1 / 15)
-      const y = (event.offsetY * -1 / 3)
+      const y = (event.offsetY * -1 / 5)
       this.$refs.bgc.style.transform = `translate3d(${x}px, ${y}px, 0)`
     }
   }
@@ -58,11 +63,11 @@ export default {
         .layer {
             overflow: hidden;
             .bgc {
-                transition: all .8s cubic-bezier(0.1, 0.7, 0.7, 1);
+                transition: all 2s cubic-bezier(0.1, 0.7, 0.7, 1);
             }
         }
     }
-    .wrap {
+    @mixin wrap-base {
         pointer-events: none; /* 禁止wrap参与鼠标冒泡 */
         position: absolute;
         top: 0;
@@ -70,17 +75,23 @@ export default {
         width: 100%;
         height: 100%;
         background-color: rgba(255, 255, 255, .6);
+    }
+    @media all and (min-width: 768px) {
+      .wrap {
+        @include wrap-base;
         clip-path: polygon(0 0, 25% 0, 60% 100%, 0 100%);
+      }
+    }
+    @media all and (max-width: 768px) {
+      .wrap {
+        @include wrap-base;
+        // clip-path: polygon(0 0, 25% 0, 60% 100%, 0 100%);
+      }
     }
 }
 .content {
     position: relative;
-    margin: auto;
-    text-align: center;
-}
-.post {
-    width: 660px;
-    height: 400px;
-    background-color: #eeeeee;
+    margin: 100px auto 0 auto;
+    width: 1200px;
 }
 </style>
