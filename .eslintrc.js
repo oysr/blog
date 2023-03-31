b9358893c4e89bf8438a82540030cf660d8514e3
oysr <940512860@qq.com>
2023-03-31 10:50:08 +0800

    eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    es6: true
   },
   extends: [
     'plugin:vue/essential',
@@ -12,7 +14,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 当格式化一个函数时，在函数名或 function 关键字和开头的括号之间允许有空白（always：总有一个；never：不允许空格）
+    'space-before-function-paren': [2, 'never']
   },
   overrides: [
     {
